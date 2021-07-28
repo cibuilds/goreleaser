c527e9c04cbb1e91e4b90da8cf07242bb6211dee
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-07-27 21:45:31 -0400

    Add GoReleaser v0.174.1. (#159)

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 0.174/Dockerfile -t cibuilds/goreleaser:latest -t cibuilds/goreleaser:0.174.0  -t cibuilds/goreleaser:0.174 .
+docker build --file 0.174/Dockerfile -t cibuilds/goreleaser:latest -t cibuilds/goreleaser:0.174.1  -t cibuilds/goreleaser:0.174 .
